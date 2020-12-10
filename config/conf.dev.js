128d05d2389b3c809e0597d0b5c5b8b13751f4b3
zhangzhengsmiling <zhangzhengsmiling@163.com>
2020-12-10 09:36:14 +0800

    0.1.0

---
+++ config/conf.dev.js
@@ -0,0 +1,12 @@
+var PARAMSCONF = {
+    TITLE: '良渚数据门户',
+    FOOTER_RIGHT: (new Date()).getFullYear() + ' 杭州云掣科技有限公司 浙ICP备15044486号-1', // 统一页脚版权文案
+    LOGSTATICVERSION: 'v0.1.0', //
+    COMPANYLOGO: 'assets/images/logo.png', //
+    UIC_HOST: 'http://172.16.10.91/',
+    BI_PORTRAIT: 'https://das.base.shuju.aliyun.com/dashboard/view/pc.htm?spm=a2c10.workspacedashboard.0.0.7bc94666ZMJOiv&pageId=8e04d0e0-3bf1-47c5-a511-59e00acb9f50',
+    BI_ORIGIN: 'https://das.base.shuju.aliyun.com/dashboard/view/pc.htm?spm=a2c10.workspacedashboard.0.0.7bc94666ZMJOiv&pageId=2fca8d9d-9974-45e5-b510-e351f89d38f6',
+    BI_YUQING:'https://das.base.shuju.aliyun.com/dashboard/view/pc.htm?spm=a2c10.workspacedashboard.0.0.7bc94666rJ4YFI&pageId=739bba62-9f96-45d1-a657-144f642084fd',
+    OSS_URL:'https://easyv-private.oss-cn-hangzhou.aliyuncs.com/'
+  }
+  
